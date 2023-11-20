e6bbeb95dcdd6713a31d159999de77425668e2ba
Jeff H <cuk8@txstate.edu>
2023-11-20 15:10:27 -0600

    abstract send() and receive()

--- IOTdevice.py
+++ IOTdevice.py
@@ -4,3 +4,15 @@ from communicator import Communicator
 class IOTDevice(Communicator):
     def __init__(self, id):
         super().__init__(id)
+
+    def send(self, message, recipient):
+        # implement the sending with UDP here
+        return
+
+    def receive(self, encrypted_message, sender):
+        # implement the receiving with UDP here
+        return
+
+    def init_sockets():
+        # initialize 1-to-1 socket with hub here
+        return
--- hub.py
+++ hub.py
@@ -6,5 +6,16 @@ class Hub(Communicator):
         super().__init__(id)
         self.name = name
 
+    def send(self, message, recipient):
+        # implement the sending with UDP here
+        return
+
+    def receive(self, encrypted_message, sender):
+        # implement the receiving with UDP here
+        return
+
+    def init_sockets():
+        # initialize 1-to-many socket here
+
 
 hub = Hub("HUB")

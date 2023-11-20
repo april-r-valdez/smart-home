8800d1e6ce0569639c1e2aed7340b44b2e51d48a
Mandil Pradhan <jns176@txstate.edu>
2023-11-20 15:17:04 -0600

    Communication update ip/id/port

---
+++ cameraIOT.py
@@ -0,0 +1,11 @@
+from IOTdevice import IOTDevice
+
+class CameraIOT(IOTDevice):
+    status = None
+    
+    def __init__(self, id):
+        super().__init__(id)
+    
+    
+    
+    
--- communicator.py
+++ communicator.py
@@ -1,4 +1,9 @@
-class Communicator:
+class Communicator:    
+    id = None
+    ip = None
+    port = None
+    commSocket = None    
+    
     def __init__(self, id):
         # self.key = 0
         self.id = id
@@ -16,3 +21,12 @@ class Communicator:
 
     def receive(self, encrypted_message, sender):
         return
+    
+    def setIP(self, ipaddr):
+        self.ip = ipaddr
+        
+    def setPort(self, portNumber):
+        self.port = portNumber
+        
+    def setSocket(self, socket):
+        self.commSocket = socket

2e789943b74bd8a568fb5eb21b6a5f1cf51a48c2
Jeff H <cuk8@txstate.edu>
2023-11-20 15:55:57 -0600

    store authenticated devices in Hub
    + dev debug input to test the hub

--- hub.py
+++ hub.py
@@ -2,9 +2,20 @@ from communicator import Communicator
 
 
 class Hub(Communicator):
-    def __init__(self, id, name):
+    """
+    A Hub class for managing and communicating with associated IoT devices.
+
+    Sends and receive commands to/from IoT devices (and users).
+        name:  name of this hub
+        _authenticated_devices:  dict of associated devices in {device_id : device_ip} pairs
+    """
+
+    _authenticated_devices = None
+
+    def __init__(self, name):
         super().__init__(id)
         self.name = name
+        self._authenticated_devices = {}
 
     def send(self, message, recipient):
         # implement the sending with UDP here
@@ -14,8 +25,58 @@ class Hub(Communicator):
         # implement the receiving with UDP here
         return
 
-    def init_sockets():
+    def init_sockets(self):
         # initialize 1-to-many socket here
+        return
+
+    def register_device(self, device_id, device_ip):
+        """
+        Add the device to the internal list of authenticated devices.
+
+        device_id:  id of the target IoT device
+        deivce_ip:  IP address of the target IoT device
+        """
+        self._authenticated_devices[device_id] = device_ip
 
 
-hub = Hub("HUB")
+def main():
+    hub = Hub("HUB")
+
+    while True:
+        # main device loop
+        # hub.receive()
+
+        # _____for debugging uses only (input will be replaced by actual user device)_____
+        user_input = input(
+            "!!!Enter a debug command (send, receive, register, list, exit): "
+        )
+
+        if user_input == "send":
+            recipient = input("Enter recipient device ID: ")
+            message = input("Enter message to send: ")
+            hub.send(message, recipient)
+
+        elif user_input == "receive":
+            recv = input("Enter encrypted message from IoT: ")
+            message = hub.receive(recv, sender)
+
+        elif user_input == "register":
+            device_id = input("Enter device ID: ")
+            device_ip = input("Enter device IP: ")
+            hub.register_device(device_id, device_ip)
+            print(f"!!!Device {device_id} registered with IP {device_ip}")
+
+        elif user_input == "exit":
+            print("!!!HUB terminated")
+            break
+
+        elif user_input == "list":
+            print(hub._authenticated_devices)
+            break
+
+        else:
+            print("Invalid command.")
+
+
+if __name__ == "__main__":
+    main()

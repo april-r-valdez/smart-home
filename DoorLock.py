293643188aac9d0b2b9ca808bc32af2aa145ec89
Brent Hoover <bah4@txstate.edu>
2023-11-21 00:55:47 -0600

    add error checks and exception handling

--- DoorLock.py
+++ DoorLock.py
@@ -12,32 +12,61 @@ class DoorLock(IOTDevice):
         self._unlock_time = "00:00"
     
     def set_state(self, state):
-        self._state = state
-        return "200"
+        if state == "on" or state == "off":    
+            self._state = state
+            return "200"
+        else:
+            return "XXXX"
     
     def set_status(self, status):
-        self._status = status
-        return "200"
+        if self._state == "off": 
+            return "XXXX"
+        if status == "lock" or status == "unlock":
+            self._status = status + "ed"
+            return "200"
+        else:
+            return "XXXX"
     
     def set_keyless_entry(self, code):
-        self._code = code
-        return "200"
+        if self._state == "off": 
+            return "XXXX"
+        if len(code) == 4 and code.isdigit():
+            self._code = code
+            return "200"
+        else:
+            return "XXXX"
         
-    def set_lock_schedule(self, lock_time):
-        self._lock_time = lock_time
-        return "200"
+    def set_lock_time(self, lock_time):
+        if self._state == "off": 
+            return "XXXX"
+        hours = lock_time[:2]
+        minutes = lock_time[3:]
+        if hours.isdigit() and minutes.isdigit() and int(hours) >= 0 and int(hours) <= 23 \
+            and int(minutes) >= 0  and int(minutes) <= 59 and len(lock_time) == 5 and lock_time[2] == ":": 
+                self._lock_time = lock_time
+                return "200"
+        else:
+            return "XXXX"
     
     def get_state(self):
+        if self._state == "off": 
+            return "XXXX"
         return self._state
     
     def get_status(self):
+        if self._state == "off": 
+            return "XXXX"
         return self._status
     
     def get_keyless_entry(self):
+        if self._state == "off": 
+            return "XXXX"
         return self._code
         
-    def get_lock_schedule(self):
-        return self._lock_time, self._unlock_time
+    def get_lock_time(self):
+        if self._state == "off": 
+            return "XXXX"
+        return self._lock_time
     
     def process_command(self, command, message=None):
         try:
@@ -45,21 +74,18 @@ class DoorLock(IOTDevice):
                 'set_state': self.set_state,
                 'set_status': self.set_status,
                 'set_keyless_entry': self.set_keyless_entry,
-                'set_lock_schedule': self.set_lock_schedule,
+                'set_lock_time': self.set_lock_time,
                 'get_state': self.get_state,
                 'get_status': self.get_status,
                 'get_keyless_entry': self.get_keyless_entry,
-                'get_lock_schedule': self.get_lock_schedule,
+                'get_lock_time': self.get_lock_time,
             }
             return mapper[command](message) if message else mapper[command]()
         
         except TypeError as e:
             print("ERROR: ", e)
-            
         except Exception as e:
             print(f"ERROR: {e} command not defined")
-            
-        return "XXXX"
 
 def main():
     lock = DoorLock(1111)    
@@ -73,78 +99,12 @@ def main():
     #     command = lock.receive()
     
     
-    # command = "set_state; on"
-    # command = "set_state;on"
-    # command = "get_state"
-    # command = "set_lock_schedule; 12:00"
-    
     command = input("Enter command: ")
     while command != "exit":
-    
-        
         command, message = lock.parse_command(command)
-        # new_command = command
-        # message = None
-        # if command.find(";") != -1:
-        #     new_command = command.split(';')[0]
-        #     if len(command.split(";")) == 2: 
-        #         message = command.split(";")[1].strip()
-        
         output = lock.process_command(command, message)
         print(output)
-        
         command = input("Enter another command: ")
-    
-    
-   
-    # message = input("Turn on device: ")
-    # if message != "on":
-    #     print("Unable to process command. Device currently off")
-    #     return
-    
-    # while message != "off" and message != "exit":
-        
-    #     if message == "on" or message == "off":
-    #         lock.set_state(message)
-    #         print("Set state: " + message)
-            
-    #     elif message == "lock" or message == "unlock":
-    #         lock.set_status(message + "ed")
-    #         print("Set status: " + message)
-            
-    #     elif message[:17] == "set keyless entry":
-    #         code = message[18:]
-    #         if len(code) != 4 or not code.isdigit():
-    #             print("Keyless entry code not set. Must contain 4 integer values.")
-    #         else:
-    #             lock.set_keyless_entry(code)
-    #             print("Set keyless entry: " + code)
-        
-    #     elif message[:17] == "set lock schedule":
-    #         print(message)
-            
-    #     elif message == "get state":
-    #         print("Get state: " + lock.get_state())
-            
-    #     elif message == "get status":
-    #         print("Get status: " + lock.get_status())
-            
-    #     elif message == "get keyless entry":
-    #         print("Get keyless entry: " + lock.get_keyless_entry())
-            
-    #     elif message == "get lock schedule":
-    #         lock_time = lock.get_lock_schedule()
-    #         print("Get lock schedule: " + lock_time)
-            
-    #     elif message == "exit":
-    #         print("Exiting device")
-        
-    #     else:
-    #         print("Command not found")
-         
-    #     message = input("Enter another command:")  
-            
-    
     
     
 if __name__ == '__main__': 

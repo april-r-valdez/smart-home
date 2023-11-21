8c5cf4f7a54716a7f2b8c74e7c2bcec2e3976b1b
Brent Hoover <bah4@txstate.edu>
2023-11-21 14:53:25 -0600

    add exceptions for invalid input

--- DoorLock.py
+++ DoorLock.py
@@ -16,29 +16,30 @@ class DoorLock(IOTDevice):
             self._state = state
             return "200"
         else:
-            return "XXXX"
+            raise Exception("invalid message", state)
+            
     
     def set_status(self, status):
         if self._state == "off": 
-            return "XXXX"
+             raise Exception("off")
         if status == "lock" or status == "unlock":
             self._status = status + "ed"
             return "200"
         else:
-            return "XXXX"
+            raise Exception("invalid message", status)
     
     def set_keyless_entry(self, code):
         if self._state == "off": 
-            return "XXXX"
+           raise Exception("off")
         if len(code) == 4 and code.isdigit():
             self._code = code
             return "200"
         else:
-            return "XXXX"
+            raise Exception("invalid message", code)
         
     def set_lock_time(self, lock_time):
         if self._state == "off": 
-            return "XXXX"
+            raise Exception("off")
         hours = lock_time[:2]
         minutes = lock_time[3:]
         if hours.isdigit() and minutes.isdigit() and int(hours) >= 0 and int(hours) <= 23 \
@@ -46,26 +47,26 @@ class DoorLock(IOTDevice):
                 self._lock_time = lock_time
                 return "200"
         else:
-            return "XXXX"
+            raise Exception("invalid message", lock_time)
     
     def get_state(self):
         if self._state == "off": 
-            return "XXXX"
+            raise Exception("off")
         return self._state
     
     def get_status(self):
         if self._state == "off": 
-            return "XXXX"
+            raise Exception("off")
         return self._status
     
     def get_keyless_entry(self):
         if self._state == "off": 
-            return "XXXX"
+            raise Exception("off")
         return self._code
         
     def get_lock_time(self):
         if self._state == "off": 
-            return "XXXX"
+            raise Exception("off")
         return self._lock_time
     
     def process_command(self, command, message=None):
@@ -85,7 +86,14 @@ class DoorLock(IOTDevice):
         except TypeError as e:
             print("ERROR: ", e)
         except Exception as e:
-            print(f"ERROR: {e} command not defined")
+            exception = e.args[0]
+            if exception == "off":
+                print("ERROR: Device currently off")
+            elif exception == "invalid message":
+                print(f"ERROR: '{e.args[1]}' message not valid")
+            else:
+                print(f"ERROR: {e} command not defined")
+            
 
 def main():
     lock = DoorLock(1111)    

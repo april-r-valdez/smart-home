da4d27f4e24811c8e85f6c867a2f24635f81a2fc
Brent Hoover <bah4@txstate.edu>
2023-11-27 01:37:51 -0600

    add comments

--- doorlockIOT.py
+++ doorlockIOT.py
@@ -4,39 +4,40 @@ from IOTdevice import IOTDevice
 class DoorLock(IOTDevice):
     def __init__(self, id):
         super().__init__(id)
-        self.id = id
         self._state = "off"
         self._status = "unlocked"
         self._code = "0000"
         self._lock_time = "00:00"
-        self._unlock_time = "00:00"
     
+    # To set state, pass 'on' or 'off' message
     def set_state(self, state):
         if state == "on" or state == "off":    
             self._state = state
-            return "200"
+            return "200" # success code
         else:
             raise Exception("invalid message", state)
             
-    
+    # To set status, pass 'lock' or 'unlock' message while device state set to on
     def set_status(self, status):
         if self._state == "off": 
              raise Exception("off")
         if status == "lock" or status == "unlock":
             self._status = status + "ed"
-            return "200"
+            return "200" # success code
         else:
             raise Exception("invalid message", status)
     
+    # To set keyless entry code, pass message in the format '0000' while device state set to on
     def set_keyless_entry(self, code):
         if self._state == "off": 
            raise Exception("off")
         if len(code) == 4 and code.isdigit():
             self._code = code
-            return "200"
+            return "200" # success code
         else:
             raise Exception("invalid message", code)
-        
+    
+    # To set lock time, pass message in the format '00:00' while device state set to on   
     def set_lock_time(self, lock_time):
         if self._state == "off": 
             raise Exception("off")
@@ -45,13 +46,11 @@ class DoorLock(IOTDevice):
         if hours.isdigit() and minutes.isdigit() and int(hours) >= 0 and int(hours) <= 23 \
             and int(minutes) >= 0  and int(minutes) <= 59 and len(lock_time) == 5 and lock_time[2] == ":": 
                 self._lock_time = lock_time
-                return "200"
+                return "200" # success code
         else:
             raise Exception("invalid message", lock_time)
     
     def get_state(self):
-        if self._state == "off": 
-            raise Exception("off")
         return self._state
     
     def get_status(self):
@@ -69,6 +68,7 @@ class DoorLock(IOTDevice):
             raise Exception("off")
         return self._lock_time
     
+    # Searches for received message from Hub and calls it's corresponding function
     def process_command(self, command, message=None):
         try:
             mapper = {
@@ -83,6 +83,7 @@ class DoorLock(IOTDevice):
             }
             return mapper[command](message) if message else mapper[command]()
         
+        # Returns error message if exception is thrown
         except TypeError as e:
             return f"ERROR: {e}"
         except Exception as e:
@@ -96,24 +97,22 @@ class DoorLock(IOTDevice):
             
 
 def main():
+    # Instantiate door lock IoT device, set encryption, and initialize socket with Hub
     lock = DoorLock(1111)
     lock.setEncryption(2, upperCaseAll=False, removeSpace=False)    
-
     lock.init_sockets("192.168.2.5", 8080)
+    
+    # Receive, parse, and process command from Hub
     command = lock.receive()
     while command != "exit":
         command, message = lock.parse_command(command)    
         output = lock.process_command(command, message)
+        
+        # Send returned output to Hub
         lock.send(output, ("192.168.2.8", 8080))
+        
+        # Continue listening for next command
         command = lock.receive()
-    
-    
-    # command = input("Enter command: ")
-    # while command != "exit":
-    #     command, message = lock.parse_command(command)
-    #     output = lock.process_command(command, message)
-    #     print(output)
-    #     command = input("Enter another command: ")
     
     
 if __name__ == '__main__': 
